bc5283c41a006bb6a567a555679ef522750f1911
sammypham <sammyphams@gmail.com>
2023-06-08 19:47:16 -0700

    SimCLR Load Functionality
    - Added SimCLR Load Functionality
    - Added scatterplot visualization for clustering.

--- src/model/simclr/simclr.py
+++ src/model/simclr/simclr.py
@@ -11,10 +11,12 @@ import numpy as np
 from dataclasses import dataclass
 import wandb
 import matplotlib.pyplot as plt
+import cv2
 
 from src.dataset.simclr_dataloader.bps_dataset import BPSMouseDataset
 from src.dataset.simclr_dataloader.augmentation import TransformsSimCLR
 from src.dataset.simclr_dataloader.bps_datamodule import BPSDataModule
+from src.dataset.augmentation import ResizeBPS
 
 import pytorch_lightning as pl
 import torch
@@ -27,6 +29,17 @@ from sklearn.preprocessing import normalize
 from lightly.data import LightlyDataset, SimCLRCollateFunction, collate
 from lightly.loss import NTXentLoss
 from lightly.models.modules.heads import SimCLRProjectionHead
+
+import matplotlib.offsetbox as osb
+import matplotlib.pyplot as plt
+
+# for resizing images to thumbnails
+import torchvision.transforms.functional as functional
+from matplotlib import rcParams as rcp
+from PIL import Image
+
+# for clustering and 2d representations
+from sklearn import random_projection
 
 @dataclass
 class BPSConfig:
@@ -65,17 +78,19 @@ class BPSConfig:
 
 
     """
-    data_dir:           str = os.path.join(root,'data','processed')
+    data_dir:           str = os.path.join(root,'Microscopy','train')
     train_meta_fname:   str = 'meta_dose_hi_hr_4_post_exposure_train.csv'
     val_meta_fname:     str = 'meta_dose_hi_hr_4_post_exposure_test.csv'
+    # train_meta_fname:   str = 'meta_train.csv'
+    # val_meta_fname:     str = 'meta_test.csv'
     save_vis_dir:       str = os.path.join(root, 'visualizations', 'simclr_knn')
     save_models_dir:    str = os.path.join(root, 'models', 'simclr')
-    batch_size:         int = 128
+    batch_size:         int = 8
     max_epochs:         int = 1
     accelerator:        str = 'auto'
     acc_devices:        int = 1
     device:             str = 'cuda' if torch.cuda.is_available() else 'cpu'
-    num_workers:        int = 8
+    num_workers:        int = 4
     dm_stage:           str = 'train'
     seed:               int = 1
 
@@ -167,6 +182,56 @@ def plot_knn_examples(embeddings, filenames, labels, n_neighbors=3, num_examples
             plt.savefig(os.path.join(root, 'visualizations', 'simclr', f'knn_example_{idx}.png'))
     #plt.savefig("knn_examples.png")
 
+def get_scatter_plot_with_thumbnails(embeddings, filenames):
+        """Creates a scatter plot with image overlays."""
+
+        # for the scatter plot we want to transform the images to a two-dimensional
+        # vector space using a random Gaussian projection
+        projection = random_projection.GaussianRandomProjection(n_components=2)
+        embeddings_2d = projection.fit_transform(embeddings)
+
+        # normalize the embeddings to fit in the [0, 1] square
+        M = np.max(embeddings_2d, axis=0)
+        m = np.min(embeddings_2d, axis=0)
+        embeddings_2d = (embeddings_2d - m) / (M - m)
+
+        
+        # initialize empty figure and add subplot
+        fig = plt.figure(figsize=(16, 16))
+        fig.suptitle("Scatter Plot of the BPS Dataset")
+        ax = fig.add_subplot(1, 1, 1)
+        # shuffle images and find out which images to show
+        shown_images_idx = []
+        shown_images = np.array([[1.0, 1.0]])
+        iterator = [i for i in range(embeddings_2d.shape[0])]
+        np.random.shuffle(iterator)
+        for i in iterator:
+            # only show image if it is sufficiently far away from the others
+            dist = np.sum((embeddings_2d[i] - shown_images) ** 2, 1)
+            if np.min(dist) < 2e-3:
+                continue
+            shown_images = np.r_[shown_images, [embeddings_2d[i]]]
+            shown_images_idx.append(i)
+
+        # plot image overlays
+        for idx in shown_images_idx:
+            thumbnail_size = int(rcp["figure.figsize"][0] * 2.0)
+            path = os.path.join("Microscopy/train", filenames[idx])
+            img = Image.open(path)
+            img = cv2.resize(np.array(img), (48, 48)) #functional.resize(img, thumbnail_size)
+            img = np.array(img)
+            img_box = osb.AnnotationBbox(
+                osb.OffsetImage(img), # osb.OffsetImage(img, cmap=plt.cm.gray_r),
+                embeddings_2d[idx],
+                pad=0.2,
+            )
+            ax.add_artist(img_box)
+
+        # set aspect ratio
+        ratio = 1.0 / ax.get_data_ratio()
+        ax.set_aspect(ratio, adjustable="box")
+        plt.savefig(os.path.join(root, 'visualizations', 'simclr', f'scatterplot_example.png'))
+
 def main():
     config = BPSConfig()
     pl.seed_everything(config.seed)
@@ -184,31 +249,51 @@ def main():
     bps_datamodule.setup(stage='validate')
     
     
-    wandb.init(project="SimCLR",
-               dir=config.save_vis_dir,
-               config=
-               {
-                   "architecture": "SimCLR",
-                   "dataset": "BPS Microscopy"
-               })
+    # wandb.init(project="SimCLR",
+    #            dir=config.save_vis_dir,
+    #            config=
+    #            {
+    #                "architecture": "SimCLR",
+    #                "dataset": "BPS Microscopy"
+    #            })
+    # model = SimCLR()
+    # trainer = pl.Trainer(max_epochs=config.max_epochs, devices=1, accelerator=config.accelerator)
+    # trainer.fit(model, bps_datamodule.train_dataloader())
+    # model.eval()
+    # embeddings, filenames, labels = generate_embeddings(model, bps_datamodule.val_dataloader())
+    # print(f'embeddings.shape: {embeddings.shape}')
+    # print(f'len(filenames): {len(filenames)}')
+    # print(f'len(labels): {len(labels)}')
+    # plot_knn_examples(embeddings, filenames, labels)
+    # wandb.finish()
+
+    # # You could use the pretrained model and train a classifier on top.
+    # pretrained_resnet_backbone = model.backbone
+    
+    # # you can also store the backbone and use it in another code
+    # state_dict = {"resnet18_parameters": pretrained_resnet_backbone.state_dict()}
+    # torch.save(state_dict, os.path.join(config.save_models_dir, "model.pth"))
+
+    ###################################################################################
+
+    # Create an instance of the SimCLR model
     model = SimCLR()
-    trainer = pl.Trainer(max_epochs=config.max_epochs, devices=1, accelerator=config.accelerator)
-    trainer.fit(model, bps_datamodule.train_dataloader())
-    model.eval()
+
+    # Load the saved state dictionary
+    state_dict = torch.load(os.path.join(config.save_models_dir, "model.pth"))
+
+    print("LOADING MODEL...")
+    # Load the backbone parameters into the SimCLR model
+    model.backbone.load_state_dict(state_dict["resnet18_parameters"])
+    print("MODEL LOADED")
+
     embeddings, filenames, labels = generate_embeddings(model, bps_datamodule.val_dataloader())
     print(f'embeddings.shape: {embeddings.shape}')
     print(f'len(filenames): {len(filenames)}')
     print(f'len(labels): {len(labels)}')
-    plot_knn_examples(embeddings, filenames, labels)
-    wandb.finish()
-
-    # You could use the pretrained model and train a classifier on top.
-    pretrained_resnet_backbone = model.backbone
     
-    # you can also store the backbone and use it in another code
-    state_dict = {"resnet18_parameters": pretrained_resnet_backbone.state_dict()}
-    torch.save(state_dict, os.path.join(config.save_models_dir, "model.pth"))
-
+    # get a scatter plot with thumbnail overlays
+    get_scatter_plot_with_thumbnails(embeddings, filenames)
 
 if __name__ == "__main__":
     main()
